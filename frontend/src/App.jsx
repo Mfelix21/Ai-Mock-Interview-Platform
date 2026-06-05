3d73ec2e974b7a6a90e6ae9677c688beef48cd66
Malcolm Felix <Mfelix2@pvamu.edu>
2026-06-05 14:15:57 -0500

    Create landing page and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [showQuestions, setShowQuestions] = useState(false);
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/questions")
@@ -13,19 +14,51 @@ function App() {
 
   return (
     <div className="app">
-      <h1>AI Mock Interview Platform</h1>
-      <p className="subtitle">Practice software engineering interviews with AI-powered questions.</p>
+      {!showQuestions ? (
+        <section className="hero">
+          <h1>AI Mock Interview Platform</h1>
+          <p>
+            Practice technical interviews with role-specific questions and AI-powered feedback.
+          </p>
+          <button onClick={() => setShowQuestions(true)}>
+            Start Interview
+          </button>
+        </section>
+      ) : (
+        <section>
+          <h1>Interview Practice</h1>
+          <h2>Interview Questions</h2>
 
-      <h2>Interview Questions</h2>
-
-      <div className="question-grid">
-        {questions.map((question, index) => (
-          <div className="question-card" key={index}>
-            <span>Question {index + 1}</span>
-            <p>{question}</p>
+          <div className="question-grid">
+            {questions.map((question, index) => (
+              <div className="question-card" key={index}>
+                <span>Question {index + 1}</span>
+                <p>{question}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+
+          <button className="back-button" onClick={() => setShowQuestions(false)}>
+            Back to Home
+          </button>
+        </section>
+      )}
+      <footer className="footer">
+  <h3>AI Mock Interview Platform</h3>
+  <p>React • FastAPI • REST APIs</p>
+
+  <div className="footer-links">
+    <a href="https://github.com/Mfelix21" target="_blank" rel="noreferrer">
+      GitHub
+    </a>
+
+    <a href="https://www.linkedin.com/in/malcolm-felix-91140a250/" target="_blank" rel="noreferrer">
+      LinkedIn
+    </a>
+  </div>
+
+  <p>Built by Malcolm Felix © 2026</p>
+</footer>
     </div>
   );
 }
